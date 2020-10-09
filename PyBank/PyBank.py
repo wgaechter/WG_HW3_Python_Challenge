60df2bcbc60755f9ad24564d3e3d0f8fcd293f66
will <will.gaechter@gmail.com>
2020-10-09 15:53:10 -0400

    save update

--- PyBank/PyBank.py
+++ PyBank/PyBank.py
@@ -1,9 +1,29 @@
 import os
 import csv
-import pandas as pd
 
 csvpath = os.path.join('Resources', 'budget_data.csv')
 
-with open(csvpath) as csvfile:
+with open(csvpath, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
+    next(csvreader)
 
+    #month_list = (list(csvreader))
+    #total_months = len(month_list)
+
+    for row in csvreader:
+        month_list = list(row[0])
+        total_months = len()
+
+    total = 0
+    for row in csvreader:
+        total = total + int(row[1])
+
+    
+
+
+
+    #print(f"Total Months: {total_months}")
+    print(f"Total Profit: ${total}")
+    #print(f"Average Profit: {mean_profit}")
+    #print(f"High Profit: ${max_profit}")
+    #print(f"Low Profit: {min_profit}")
--- PyPoll/PyPoll.py
+++ PyPoll/PyPoll.py
@@ -1,1 +1,11 @@
 import os
+import csv
+
+election_csv = os.path.join('Resources', 'election_data.csv')
+
+with open(election_csv, 'r') as csvfile:
+    csvreader = csv.DictReader(csvfile, delimiter=',')
+    
+    header = next(csvreader)
+
+    print(header)
